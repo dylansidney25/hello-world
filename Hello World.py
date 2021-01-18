bf21522a759e8855fd5bc1d621a35bc55e88e566
dylansidney25 <77400473+dylansidney25@users.noreply.github.com>
2021-01-18 10:30:30 -0400

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,30 @@
+"""/**************************************************************
+This is the first part of the training course of Python
+Instructor: Ricardo Bautista Quintero
+Date: January 18th 2021
+NSCC Ivany Campus
+/*****************************************************************
+"""
+"""/**************************************************************
+We will learn about the following topics:
+1.) Types of numbers in Python
+2.) Basic Arithmetic
+3.) Differences between clasic devision and floor devision
+4.) Object Assignment in Python
+/*****************************************************************
+"""
+
+def program_1():
+    a = 2
+    f = 2e4
+    
+    print(a)
+    print("is an integer")
+    print(f)
+    print("is a floating point")
+    
+if __name__ == '__main__':
+    program_1()
+
+    
+    
